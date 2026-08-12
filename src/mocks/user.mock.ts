e460c91b69aa7d7e6023f5dd194bc8197141564e
gustavo martins <luizgustams@gmail.com>
2026-08-12 13:50:17 -0300

    refactor: add type phone in user register

--- src/mocks/user.mock.ts
+++ src/mocks/user.mock.ts
@@ -5,6 +5,7 @@ export const users: User[] = [
 		id: 1,
 		name: 'Gustavo Martins',
 		email: 'luiz.silva@gmail.com',
+		phone: '(85) 99856-8223',
 		password: 'a1b2c3d4e5',
 		created_at: '2026-08-11',
 		updated_at: '2026-08-11'
--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,7 @@ export type User = {
 	id: number;
 	name: string;
 	email: string;
+	phone: string;
 	password: string;
 	created_at: string;
 	updated_at: string;
