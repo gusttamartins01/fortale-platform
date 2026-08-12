9c3153e0a09e74806200f933abbfcc3f47d53953
gustavo martins <luizgustams@gmail.com>
2026-08-12 14:42:48 -0300

    refactor: format errors to obtain structured data and messages in Portuguese.

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,8 +1,37 @@
 import { z } from 'zod';
 
 export const createUserSchema = z.object({
-	name: z.string().min(1),
-	email: z.email(),
-	phone: z.string().min(1).max(15),
-	password: z.string().min(5).max(10)
+	name: z
+		.string('Entrada inválida: Esperava-se um texto.')
+		.min(1, 'Muito curto: esperava-se um texto com ao menos um caractere.'),
+	email: z.email('Endereço de e-mail inválido.'),
+	phone: z
+		.string('Entrada inválida: Esperava-se um texto.')
+		.min(1, 'Muito curto: esperava-se um texto com ao menos um caractere.')
+		.max(
+			15,
+			'Limite atingido: esperava-se um texto com menos de 15 caracteres'
+		),
+	password: z
+		.string('Entrada inválida: Esperava-se um texto.')
+		.min(5, 'Muito curto: esperava-se um texto com ao menos 5 caracteres.')
+		.max(10, 'Limite atingido: esperava-se um texto com menos de 10 caracteres')
+});
+
+export const updateUserSchema = z.object({
+	name: z
+		.string('Entrada inválida: Esperava-se um texto.')
+		.min(1, 'Muito curto: esperava-se um texto com ao menos um caractere.')
+		.optional(),
+	email: z.email('Endereço de e-mail inválido.').optional(),
+	phone: z
+		.string('Entrada inválida: Esperava-se um texto.')
+		.min(1, 'Muito curto: esperava-se um texto com ao menos um caractere.')
+		.max(15, 'Limite atingido: esperava-se um texto com menos de 15 caracteres')
+		.optional(),
+	password: z
+		.string('Entrada inválida: Esperava-se um texto.')
+		.min(5, 'Muito curto: esperava-se um texto com ao menos 5 caracteres.')
+		.max(10, 'Limite atingido: esperava-se um texto com menos de 10 caracteres')
+		.optional()
 });
