b8adfea81b29d8f84f55682a14e0dde0528c8e54
gustavo martins <luizgustams@gmail.com>
2026-08-11 23:30:15 -0300

    feat: add mocked user data

---
+++ src/mocks/user.mock.ts
@@ -0,0 +1,12 @@
+import type { User } from '../types.ts';
+
+export const users: User[] = [
+	{
+		id: 1,
+		name: 'Gustavo Martins',
+		email: 'luiz.silva@gmail.com',
+		password: 'a1b2c3d4e5',
+		created_at: '2026-08-11',
+		updated_at: '2026-08-11'
+	}
+];
