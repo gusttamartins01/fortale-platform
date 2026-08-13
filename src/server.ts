8713488ab67febf3890bdcb8e678f7244ef1203e
gustavo martins <luizgustams@gmail.com>
2026-08-13 02:33:49 -0300

    Refactor: update request logger.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import express from 'express';
+import { pinoHttp } from 'pino-http';
+import logger from './lib/logger.ts';
+import errorHandler from './middlewares/errorHandler.ts';
+import userRoute from './routes/user.route.ts';
 
 const app = express();
 
+app.use(pinoHttp({ logger }));
+
 app.use(express.json());
 
-app.use('/users', (_request, response) => {
-	response.status(200).json({
-		message: 'Hello, world!'
-	});
-});
+app.use('/users', userRoute);
 
 app.use((_request, response) => {
 	response.status(400).json({
@@ -16,8 +18,10 @@ app.use((_request, response) => {
 	});
 });
 
+app.use(errorHandler);
+
 const PORT = Number(process.env.PORT);
 
 app.listen(PORT, () => {
-	console.log(`Sever running on port: http://localhost:${PORT}`);
+	logger.info(`Sever running on port: http://localhost:${PORT}`);
 });
