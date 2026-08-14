000e64bdb3d18c360a1a2ca8f615dffcc35a1d0d
gustavo martins <luizgustams@gmail.com>
2026-08-14 16:47:45 -0300

    add reusable types of categories  and places.

--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,27 @@ export type ValidationFieldError = {
 	field: string;
 	message: string;
 };
+
+export type Category = {
+	id: number;
+	name: string;
+	description: string;
+	created_at: string;
+};
+export type Place = {
+	id: number;
+	name: string;
+	description: string;
+	address: string;
+	neighborhood: string;
+	latitude: number;
+	longitude: number;
+	phone: string;
+	website: string;
+	instagram: string;
+	opening_hours: string;
+	price_ranger: string;
+	is_active: boolean;
+	created_at: string;
+	update_at: string;
+};
