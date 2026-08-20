a1126ae2fdfb51dd9ba5ea945357bc877f063047
gustavo martins <luizgustams@gmail.com>
2026-08-20 00:22:59 -0300

    refactor: Add category http route to main server.

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { pinoHttp } from 'pino-http';
 import logger from './lib/logger.ts';
 import errorHandler from './middlewares/errorHandler.ts';
+import categoryRoute from './routes/category.route.ts';
 import userRoute from './routes/user.route.ts';
 
 const app = express();
@@ -11,9 +12,10 @@ app.use(pinoHttp({ logger }));
 app.use(express.json());
 
 app.use('/users', userRoute);
+app.use('/categories', categoryRoute);
 
 app.use((_request, response) => {
-	response.status(400).json({
+	response.status(404).json({
 		message: 'Page not found!'
 	});
 });
