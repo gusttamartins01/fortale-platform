ea152c16be3d62eb4071482b1088ee7022d5a1f7
gustavo martins <luizgustams@gmail.com>
2026-08-20 00:28:17 -0300

    feat: add data mocks from the category.

---
+++ src/mocks/category.mock.ts
@@ -0,0 +1,11 @@
+import type { Category } from '../types.ts';
+
+export const categories: Category[] = [
+	{
+		id: 1,
+		name: 'Cafeterias',
+		description:
+			'Encontre cafeterias e espaços aconchegantes para tomar um café e aproveitar momentos especiais em Fortaleza.',
+		created_at: '2026-08-19'
+	}
+];
