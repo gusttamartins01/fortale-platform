58e1f1c1c92d1ef78851b09862dc99b7287d3583
gustavo martins <luizgustams@gmail.com>
2026-08-26 22:35:49 -0300

    feat: include roles route in the main service.

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { pinoHttp } from 'pino-http';
 import logger from './lib/logger.ts';
 import errorHandler from './middlewares/errorHandler.ts';
 import categoryRoute from './routes/category.route.ts';
+import roleRoute from './routes/role.route.ts';
 import userRoute from './routes/user.route.ts';
 
 const app = express();
@@ -11,6 +12,7 @@ app.use(pinoHttp({ logger }));
 
 app.use(express.json());
 
+app.use('/roles', roleRoute);
 app.use('/users', userRoute);
 app.use('/categories', categoryRoute);
 
