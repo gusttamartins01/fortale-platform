5e6b68e3d92b094352629cb71e173f9887068657
gustavo martins <luizgustams@gmail.com>
2026-08-28 12:08:41 -0300

    feat: Implement the logic for the roles router  in the system.

--- src/routes/role.route.ts
+++ src/routes/role.route.ts
@@ -5,7 +5,7 @@ import { createRoleSchema, updateRoleSchema } from '../schemas/role.schema.ts';
 
 const router = Router();
 
-router.get('/', RoleController.getAllRole);
+router.get('/', RoleController.getAllRoles);
 router.get('/:id', RoleController.getRoleById);
 router.post('/', validate(createRoleSchema), RoleController.createRole);
 router.put('/:id', validate(updateRoleSchema), RoleController.updateRole);
