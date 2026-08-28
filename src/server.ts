a0c5a77ac638dc72e8f97e1b85d7583bb8f2405b
gustavo martins <luizgustams@gmail.com>
2026-08-28 12:16:21 -0300

    refactor: include router place, role and event in main server.

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import { pinoHttp } from 'pino-http';
 import logger from './lib/logger.ts';
 import errorHandler from './middlewares/errorHandler.ts';
 import categoryRoute from './routes/category.route.ts';
+import eventRouter from './routes/event.route.ts';
+import placeRoute from './routes/place.route.ts';
 import roleRoute from './routes/role.route.ts';
 import userRoute from './routes/user.route.ts';
 
@@ -15,6 +17,8 @@ app.use(express.json());
 app.use('/roles', roleRoute);
 app.use('/users', userRoute);
 app.use('/categories', categoryRoute);
+app.use('/places', placeRoute);
+app.use('/events', eventRouter);
 
 app.use((_request, response) => {
 	response.status(404).json({
