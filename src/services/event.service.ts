c7e8a557c567a6a84161f6312c362d0657eac2a9
gustavo martins <luizgustams@gmail.com>
2026-08-28 12:06:12 -0300

    feat: Implement the logic for the places services in the system.

---
+++ src/services/event.service.ts
@@ -0,0 +1,43 @@
+import { NotFoundError } from '../errors/index.ts';
+import { prisma } from '../lib/prisma.ts';
+import type { CreateEvent, UpdateEvent } from '../schemas/event.schema.ts';
+import type { Event } from '../types.ts';
+
+export async function findAllEvents(): Promise<Event[]> {
+	const events = await prisma.event.findMany();
+
+	return events;
+}
+
+export async function findEventById(id: number): Promise<Event> {
+	const event = await prisma.event.findUnique({
+		where: { id }
+	});
+
+	if (!event) throw new NotFoundError(`Event with ID ${id} not found.`);
+
+	return event;
+}
+
+export async function insertEvent(data: CreateEvent): Promise<Event> {
+	return await prisma.event.create({
+		data
+	});
+}
+
+export async function modifyEvent(id: number, data: UpdateEvent) {
+	await findEventById(id);
+
+	return await prisma.event.update({
+		where: { id },
+		data
+	});
+}
+
+export async function removeEvent(id: number): Promise<void> {
+	await findEventById(id);
+
+	await prisma.event.delete({
+		where: { id }
+	});
+}
